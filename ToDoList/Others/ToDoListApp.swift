6d6d2c916c8610da94a15c1d2d62df1e6503c69a
varadasainikhil <varadasainikhil@gmail.com>
2025-06-17 22:28:58 -0400

    - added firebase SDK - configured firebase through firebase core - added googlesevice-info plist from firebase - created folders for future purposes

---
+++ ToDoList/Others/ToDoListApp.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sai Nikhil Varada on 6/17/25.
+//
+
+import FirebaseCore
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct ToDoListApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
