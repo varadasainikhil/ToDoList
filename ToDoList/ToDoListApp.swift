8a4d5d2784721bd7a95086b1a1c2a40ac5b64bfc
varadasainikhil <varadasainikhil@gmail.com>
2025-06-17 22:09:39 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sai Nikhil Varada on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
